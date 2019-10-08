2d7966b11d1644c72408c463825322fed9d31d43
Dustin <dustinfranz15@gmail.com>
2019-10-07 20:27:16 -0500

    commit

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -1,1 +1,9 @@
-select 1
+
+CREATE PROCEDURE dbo.StoredProcedureName
+    @param1 /*parameter name*/ int /*datatype_for_param1*/ = 0, /*default_value_for_param1*/
+    @param2 /*parameter name*/ int /*datatype_for_param1*/ = 0 /*default_value_for_param2*/
+-- add more stored procedure parameters here
+AS
+    -- body of the stored procedure
+    SELECT @param1, @param2
+GO
